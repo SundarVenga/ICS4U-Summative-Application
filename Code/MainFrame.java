a25953608714a877a77c390d5831e733ba4291ca
therealvenga22 <44201293+SundarVenga@users.noreply.github.com>
2019-01-21 14:02:23 -0500

    Added dropdown menu options for plans

--- Code/MainFrame.java
+++ Code/MainFrame.java
@@ -57,14 +57,134 @@ public class MainFrame{
 				}
 			}
 		});
-    	/**
+	}
+	/**
 	 * Create the application.
 	 */
 	public MainFrame() {
 		initialize();
 	}
-  
-  	private void initialize() {
+	String budget;
+	String plan;
+	String tab;
+	static int count;
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	
+	public void checker() {
+		if(budget == "Below $50") {
+			if(plan == "Data + Talk + Text") {
+				if(tab == "Yes") {
+					count = 1;
+				}
+				else {
+					count = -1;
+				}
+			}
+			if(plan == "Data Only") {
+				if(tab == "Yes") {
+					count = 2;
+				}
+				else {
+					count = -2;
+				}
+			}
+			if(plan == "Talk + Text") {
+				if(tab == "Yes") {
+					count = 3;
+				}
+				else {
+					count = -3;
+				}
+			}
+		}
+		
+		if(budget == "$50 - $90") {
+			if(plan == "Data + Talk + Text") {
+				if(tab == "Yes") {
+					count = 4;
+				}
+				else {
+					count = -4;
+				}
+			}
+			if(plan == "Data Only") {
+				if(tab == "Yes") {
+					count = 5;
+				}
+				else {
+					count = -5;
+				}
+			}
+			if(plan == "Talk + Text") {
+				if(tab == "Yes") {
+					count = 6;
+				}
+				else {
+					count = -6;
+				}
+			}
+		}
+		
+		if(budget == "$90 - $130") {
+			if(plan == "Data + Talk + Text") {
+				if(tab == "Yes") {
+					count = 7;
+				}
+				else {
+					count = -7;
+				}
+			}
+			if(plan == "Data Only") {
+				if(tab == "Yes") {
+					count = 8;
+				}
+				else {
+					count = -8;
+				}
+			}
+			if(plan == "Talk + Text") {
+				if(tab == "Yes") {
+					count = 9;
+				}
+				else {
+					count = -9;
+				}
+			}
+		}
+		
+		if(budget == "$130+") {
+			if(plan == "Data + Talk + Text") {
+				if(tab == "Yes") {
+					count = 10;
+				}
+				else {
+					count = -10;
+				}
+			}
+			if(plan == "Data Only") {
+				if(tab == "Yes") {
+					count = 11;
+				}
+				else {
+					count = -11;
+				}
+			}
+			if(plan == "Talk + Text") {
+				if(tab == "Yes") {
+					count = 12;
+				}
+				else {
+					count = -12;
+				}
+			}
+		}
+		
+	}
+	
+	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 550, 450);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -93,6 +213,19 @@ public class MainFrame{
 		txtrWhatIsYour.setBounds(25, 217, 129, 40);
 		frame.getContentPane().add(txtrWhatIsYour);
 		
+		JComboBox budgetPick = new JComboBox();
+		budgetPick.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//JComboBox cb = (JComboBox)e.getSource();
+				budget = (budgetPick.getSelectedItem()).toString();
+			}
+		}); 
+		budgetPick.setModel(new DefaultComboBoxModel(new String[] {"Choose", "Below $50", "$50 - $90", "$90 - $130", "$130+"}));
+		budgetPick.setToolTipText("");
+		budgetPick.setBounds(25, 269, 129, 26);
+		frame.getContentPane().add(budgetPick);
+		Image img = new ImageIcon(this.getClass().getResource("/bg1.jpg")).getImage();
+		
 		txtrWhatTypeOf = new JTextArea();
 		txtrWhatTypeOf.setText("What type of\nplan do you\nprefer?");
 		txtrWhatTypeOf.setEditable(false);
@@ -104,10 +237,55 @@ public class MainFrame{
 		txtrWhatType.setBounds(266, 160, -72, 16);
 		frame.getContentPane().add(txtrWhatType);
 		
+		planPick = new JComboBox();
+		planPick.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//JComboBox cb = (JComboBox)e.getSource();
+				plan = (planPick.getSelectedItem()).toString();
+			}
+		});
+		planPick.setModel(new DefaultComboBoxModel(new String[] {"Choose", "Data + Talk + Text", "Data Only", "Talk + Text"}));
+		planPick.setToolTipText("");
+		planPick.setBounds(206, 269, 129, 26);
+		frame.getContentPane().add(planPick);
+		
 		txtrDoYouNeed = new JTextArea();
 		txtrDoYouNeed.setText("Do you need\na cellphone\nto come with\nthe plan?");
 		txtrDoYouNeed.setEditable(false);
 		txtrDoYouNeed.setBounds(392, 184, 129, 71);
 		frame.getContentPane().add(txtrDoYouNeed);
 		
-    }
+		tabPick = new JComboBox();
+		tabPick.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//JComboBox cb = (JComboBox)e.getSource();
+				tab = (tabPick.getSelectedItem()).toString();
+			}
+		});
+		tabPick.setModel(new DefaultComboBoxModel(new String[] {"Choose", "Yes", "No"}));
+		tabPick.setToolTipText("");
+		tabPick.setBounds(392, 269, 129, 26);
+		frame.getContentPane().add(tabPick);
+		
+		JButton btnContinue = new JButton("Continue");
+		btnContinue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {	
+			
+			checker();
+			if(count<=12) {
+				MainFrame.frame.dispose();
+				Plans pln = new Plans();
+				pln.setVisible(true);
+			}
+
+			}
+		});
+		btnContinue.setBounds(206, 338, 117, 29);
+		frame.getContentPane().add(btnContinue);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(img));
+		label.setBounds(6, 6, 538, 416);
+		frame.getContentPane().add(label);
+	}
+}
